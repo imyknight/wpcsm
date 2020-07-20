72215557eb5906f834b7dd7189c05c4e6c7f4957
hung <Hung@Macbook-Pro.local>
2020-07-20 14:44:38 +0700

    config ok

---
+++ src/templates/BlogPost.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { graphql } from "gatsby"
+
+const BlogPostTemplate = ({ data }) => (
+  <Layout>
+    <SEO
+      title={data.wordpressPost.title}
+      description={data.wordpressPost.excerpt}
+    />
+    <h1>{data.wordpressPost.title}</h1>
+    <p>
+      Written by {data.wordpressPost.author.name} on {data.wordpressPost.date}
+    </p>
+    <div
+      style={{ marginTop: 20 }}
+      dangerouslySetInnerHTML={{ __html: data.wordpressPost.content }}
+    />
+  </Layout>
+)
+export default BlogPostTemplate
+
+export const query = graphql`
+  query($id: Int!) {
+    wordpressPost (wordpress_id: { eq: $id })  {
+    content
+    excerpt
+    date
+    author {
+      name
+    }
+  }
+  }
+`
